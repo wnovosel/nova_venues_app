6d391d861d34c22f3ea1f0b781b915f06936236c
wnovosel <walt@novocellars.com>
2026-06-30 08:19:08 -0400

    fix: stop committing Pods - let CI install fresh every build

--- ios/Pods/PostHog/vendor/PHPLCrashReporter/Source/CrashReporterFramework.m
+++
@@ -1,5 +0,0 @@
-/*
- * This empty file is required to correctly resolve
- * all build settings and dependencies in the framework targets.
- */
-__attribute__((used, visibility("hidden"))) const int PHPLCrashReporterFrameworkTranslationUnitAnchor = 0;
